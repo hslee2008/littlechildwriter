f6f91acda2d119f4866f454d78ec272d0ff181fe
Hyunseung Lee <hyunseunglee2008@gmail.com>
2022-09-16 14:22:05 +0900

    Test - search school library

---
+++ pages/schoolsearch.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="searchHTML"></div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      searchHTML: ''
+    }
+  },
+  created() {
+    fetch(
+      'https://reading.gglec.go.kr/r/newReading/search/schoolSearchResult.jsp?dataType=ALL&division1=ALL&connect1=A&searchCon1=%EC%BD%94%EC%8A%A4%EB%AA%A8%EC%8A%A4'
+    )
+      .then(res => res.text())
+      .then(html => {
+        this.searchHTML = html
+      })
+  }
+})
+</script>
